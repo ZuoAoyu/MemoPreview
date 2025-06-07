0d15de921d9e4819582b15e7c87ea9121e28fa42
aoyu <aoyu@xiake.me>
2025-06-07 23:59:08 +0800

    增加工具栏放置pdf相关操作

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,6 +2,7 @@
 #include <QComboBox>
 #include <QToolBar>
 #include <QLabel>
+#include <QPdfPageSelector>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -28,32 +29,73 @@ void MainWindow::createActions()
 
 void MainWindow::createToolBars()
 {
-    QToolBar *settingToolBar = new QToolBar;
+    {
+        QToolBar *settingToolBar = new QToolBar;
 
-    QComboBox* superWindowSelector = new QComboBox(settingToolBar);
-    superWindowSelector->setToolTip("SuperMemo 窗口选择");
-    superWindowSelector->addItem("SuperMemo 1");
-    superWindowSelector->addItem("SuperMemo 2");
-    superWindowSelector->addItem("SuperMemo 3");
-    superWindowSelector->addItem("SuperMemo 4");
-    superWindowSelector->addItem("SuperMemo 5");
-    superWindowSelector->addItem("SuperMemo 6");
+        QComboBox* superWindowSelector = new QComboBox(settingToolBar);
+        superWindowSelector->setToolTip("SuperMemo 窗口选择");
+        superWindowSelector->addItem("SuperMemo 1");
+        superWindowSelector->addItem("SuperMemo 2");
+        superWindowSelector->addItem("SuperMemo 3");
+        superWindowSelector->addItem("SuperMemo 4");
+        superWindowSelector->addItem("SuperMemo 5");
+        superWindowSelector->addItem("SuperMemo 6");
 
-    QComboBox* latexTemplateSelector = new QComboBox(settingToolBar);
-    latexTemplateSelector->setToolTip("LaTeX 模板选择");
-    latexTemplateSelector->addItem("标准");
-    latexTemplateSelector->addItem("数学");
+        QComboBox* latexTemplateSelector = new QComboBox(settingToolBar);
+        latexTemplateSelector->setToolTip("LaTeX 模板选择");
+        latexTemplateSelector->addItem("标准");
+        latexTemplateSelector->addItem("数学");
 
-    settingToolBar->addWidget(superWindowSelector);
-    settingToolBar->addWidget(latexTemplateSelector);
+        settingToolBar->addWidget(superWindowSelector);
+        settingToolBar->addSeparator(); // 插入分隔符，增加间距
+        settingToolBar->addWidget(latexTemplateSelector);
 
+        addToolBar(settingToolBar);
+    }
 
-    QToolBar *operateToolBar = new QToolBar;
-    operateToolBar->addAction(refreshAction);
-    operateToolBar->addAction(showLogAction);
-    operateToolBar->addAction(showConfigAction);
-    operateToolBar->addAction(openWorkspaceAction);
+    {
+        QToolBar *operateToolBar = new QToolBar;
+        operateToolBar->addAction(refreshAction);
+        operateToolBar->addAction(showLogAction);
+        operateToolBar->addAction(showConfigAction);
+        operateToolBar->addAction(openWorkspaceAction);
 
-    addToolBar(settingToolBar);
-    addToolBar(operateToolBar);
+        addToolBar(operateToolBar);
+    }
+
+    // 添加换行。在一个新行上显示下面的工具栏
+    addToolBarBreak();
+
+    {
+        QToolBar *pdfToolBar = new QToolBar;
+
+        QPdfPageSelector* pageSelector = new QPdfPageSelector(pdfToolBar);
+        pageSelector->setToolTip("翻页");
+        pdfToolBar->addWidget(pageSelector);
+
+        pdfToolBar->addSeparator(); // 插入分隔符，增加间距
+
+        QComboBox* zoomSelector = new QComboBox(pdfToolBar);
+        zoomSelector->setToolTip("缩放");
+        zoomSelector->addItem(tr("Fit Width"));
+        zoomSelector->addItem(tr("Fit Page"));
+        zoomSelector->addItem(tr("12%"));
+        zoomSelector->addItem(tr("25%"));
+        zoomSelector->addItem(tr("33%"));
+        zoomSelector->addItem(tr("50%"));
+        zoomSelector->addItem(tr("66%"));
+        zoomSelector->addItem(tr("75%"));
+        zoomSelector->addItem(tr("100%"));
+        zoomSelector->addItem(tr("125%"));
+        zoomSelector->addItem(tr("150%"));
+        zoomSelector->addItem(tr("200%"));
+        zoomSelector->addItem(tr("400%"));
+
+        pdfToolBar->addWidget(zoomSelector);
+
+        auto* actionZoomIn = pdfToolBar->addAction("放大", QKeySequence::ZoomIn);
+        auto* actionZoomOut = pdfToolBar->addAction("缩小", QKeySequence::ZoomOut);
+
+        addToolBar(pdfToolBar);
+    }
 }
